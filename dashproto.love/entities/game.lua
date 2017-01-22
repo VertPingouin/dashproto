0e088543cae046b62b33f37289fa6bfeaa758579
= <sylvain.boussekey@vertpingouin.fr>
2017-01-22 21:59:27 +0100

    baton test

--- dashproto.love/entities/game.lua
+++ dashproto.love/entities/game.lua
@@ -3,7 +3,17 @@
 Game = {}
 
 function Game:new()
-  local game = entity:new('game',{tags={'ticking'}})
+  local game = entity:new('game',{tags={'ticking'},order=2})
+
+  local controls = {
+    left = {'axis:leftx-', 'button:dpleft'},
+    right = {'axis:leftx+', 'button:dpright'},
+    up = {'axis:lefty-', 'button:dpup'},
+    down = {'axis:lefty+', 'button:dpdown'},
+    shoot = {'key:x', 'button:a'}
+  }
+
+  game.joy1 = baton.new('joy1','game',controls,love.joystick.getJoysticks()[1])
 
   function game:setScene(scene)
     self.currentScene = scene
