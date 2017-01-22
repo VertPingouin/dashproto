d1e0bfa8af8f1b478137d4ad7cc1db999d015c75
= <sylvain.boussekey@vertpingouin.fr>
2017-01-22 22:05:27 +0100

    renamed scene to Scene

--- dashproto.love/scenes/mainscene.lua
+++ dashproto.love/scenes/mainscene.lua
@@ -4,12 +4,8 @@ function MainScene:new(parent)
   local mainScene = entity:new('mainScene',{tags={'visible','ticking'},parent=parent})
 
   mainScene.player = player:new(self,1,1)
-  mainScene.control = control:new(self,1,1)
-
 
   function mainScene:tick(dt)
-    local newmove = self.control:getAxis(1)
-    self.player:setMovement(newmove.x,newmove.y)
   end
 
   return mainScene
