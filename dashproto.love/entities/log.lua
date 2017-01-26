28535719239bcfe2a4681d414163d3be92f72644
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-01-26 13:29:12 +0100

    give ability to send log to console ingame

--- dashproto.love/entities/log.lua
+++ dashproto.love/entities/log.lua
@@ -1,18 +1,37 @@
 local Log = {}
 
-function Log:new()
+function Log:new(dest)
   local log = {}
   entity:new('log',{tags={'ticking'}})
 
-  function log:post(level,emitter,log,dest)
+  log.console = nil
+  if dest then
+    log.console = obm:get(dest)
+  end
+
+  function log:post(level,emitter,log)
     if level == 'ERROR' then
       assert(1==2,level..' from '..emitter..' : '..log)
     elseif level == 'DEBUG' then
       if debug then
-        print(level..' from '..emitter..' : '..log)
+        if not self.console then
+          print(level..' from '..emitter..' : '..log)
+        else
+          self.console:print(level..' from '..emitter..' : '..log)
+        end
+      end
+    elseif level == 'RAW' then
+      if not self.console then
+        print(log)
+      else
+        self.console:print(log)
       end
     else
-      print(level..' from '..emitter..' : '..log)
+      if not self.console then
+        print(level..' from '..emitter..' : '..log)
+      else
+        self.console:print(level..' from '..emitter..' : '..log)
+      end
     end
   end
 
