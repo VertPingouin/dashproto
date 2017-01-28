70e2248cd966af08e8649935fb06c5c02a18ab64
= <sylvain.boussekey@vertpingouin.fr>
2017-01-28 22:49:56 +0100

    testing

--- dashproto.love/scenes/mainscene.lua
+++ dashproto.love/scenes/mainscene.lua
@@ -1,10 +1,10 @@
 MainScene = {}
 
 function MainScene:new(parent)
-  local mainScene = entity:new('mainScene',{tags={'visible','ticking'},parent=parent})
-
+  local mainScene = scene:new(parent,'mainScene')
   mainScene.player = player:new('mainScene')
-  --mainScene.player:destroy()
+  mainScene.collider = collider:new('mainScene',500,100,200,200)
+  mainScene.collider = collider:new('mainScene',400,500,200,200)
 
   function mainScene:tick(dt)
   end
