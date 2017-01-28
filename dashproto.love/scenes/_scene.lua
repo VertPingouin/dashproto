09c7d77545d532aeaad15f99c78ecc2aa1e3dd0a
= <sylvain.boussekey@vertpingouin.fr>
2017-01-28 22:49:48 +0100

    base class for scenes

---
+++ dashproto.love/scenes/_scene.lua
@@ -0,0 +1,11 @@
+Scene = {}
+function Scene:new(parent,name)
+  local scene = entity:new(name,{tags={'visible','ticking'},parent=parent})
+  scene.world = bump.newWorld(tilesize,name)
+
+  function scene:tick(dt)
+  end
+
+  return scene
+end
+return Scene
