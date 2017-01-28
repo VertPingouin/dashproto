353592ae3ed1891873082b4b0766134c01131364
= <sylvain.boussekey@vertpingouin.fr>
2017-01-28 22:50:19 +0100

    update

--- dashproto.love/requirement.lua
+++ dashproto.love/requirement.lua
@@ -6,15 +6,23 @@ renderer = require('modules/renderer')
 gameloop = require('modules/gameloop')
 
 --entities
-entity = require('entities/entity')
+entity = require('entities/_entity')
 log = require('entities/log')
 game = require('entities/game')
 player = require('entities/player')
 target = require('entities/target')
 test_entity = require('entities/test_entity')
-baton = require('entities/baton')
 console = require('entities/console')
-
+collider = require('entities/collider')
 --components
-component = (require('components/component'))
+component = require('components/_component')
 c_statemachine = require('components/c_statemachine')
+c_body = require('components/c_body')
+
+--scenes
+scene = require('scenes/_scene')
+
+--libs
+--pseudo-entities, can't have components
+bump = require('libs/bump')
+baton = require('libs/baton')
