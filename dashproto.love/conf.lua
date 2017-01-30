ae0048fc3029bf4814541f4fd7a5d6cae661535c
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-01-30 13:56:03 +0100

    add detailed debug params

--- dashproto.love/conf.lua
+++ dashproto.love/conf.lua
@@ -2,14 +2,17 @@
 function love.conf(t)
 	t.title = "Dash proto" -- The title of the window the game is in (string)
 	t.version = "0.10.0"         -- The LÖVE version this game was made for (string)
-	--t.window.borderless = true
-	t.window.width = 1024        -- we want our game to be long and thin.
+	t.window.borderless = true
+	t.window.width = 1024
 	t.window.height = 768
 
 	-- For Windows debugging
 	t.console = true
 
-	debug = true
+	debug = {
+		boxes = true,
+		loglevel = 'DEBUG' --TODO make debug level work
+	}
 	tilesize = 32
 	maxlayers = 10
 	maxorders = 5
