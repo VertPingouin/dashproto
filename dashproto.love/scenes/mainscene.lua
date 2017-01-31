a9302e4a47f89eb16a4ed5471cdb493e474368da
= <sylvain.boussekey@vertpingouin.fr>
2017-01-31 20:10:10 +0100

    imlementing map

--- dashproto.love/scenes/mainscene.lua
+++ dashproto.love/scenes/mainscene.lua
@@ -2,7 +2,7 @@ MainScene = {}
 
 function MainScene:new(parent)
   local mainScene = scene:new(parent,'mainScene')
-  mainScene.map = map:new('mainScene','mainSceneMap',{luamap=require('maps/test')})
+  mainScene.map = map:new('mainScene','mainSceneMap',{luamap=require('maps/test'),position=vec2(20,20)})
   mainScene.player = player:new('mainScene',{position=mainScene.map:getSpawn('player')})
 
   function mainScene:tick(dt)
