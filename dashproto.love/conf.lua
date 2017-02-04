460a27053906374bccf8037c6cc8bf0e166bf8f2
= <sylvain.boussekey@vertpingouin.fr>
2017-02-04 06:15:09 +0100

    testing

--- dashproto.love/conf.lua
+++ dashproto.love/conf.lua
@@ -2,7 +2,7 @@
 function love.conf(t)
 	t.title = "Dash proto" -- The title of the window the game is in (string)
 	t.version = "0.10.0"         -- The LÖVE version this game was made for (string)
-	t.window.borderless = true
+	t.window.borderless = false
 	t.window.width = 1024
 	t.window.height = 768
 
