4cb292738b4b3f2e05dc8780f085e7dc5347da53
= <sylvain.boussekey@vertpingouin.fr>
2017-02-04 06:15:00 +0100

    adding function to move and return new ccords

--- dashproto.love/components/c_body.lua
+++ dashproto.love/components/c_body.lua
@@ -9,7 +9,7 @@ function C_body:new(owner,id,a)
     {'w','defaultValue','number',16},
     {'h','defaultValue','number',16},
     {'color','defaultValue','table',color:new(255,255,255,255)},
-    {'family','mandatory','string'}
+    {'family','mandatory','string'},
   })
   a = check:check(a)
 
@@ -18,25 +18,20 @@ function C_body:new(owner,id,a)
   c_body.h = a.h
   c_body.color = a.color
   c_body.family = a.family --used to know the collision response via colm
-  c_body.name = obm:getId(owner)..'.'..id --used to detect particular collision 
+  c_body.name = obm:getId(owner)..'.'..id --used to detect particular collision
 
   assert(obm:get('bumpWorld'),'c_body::new::a world object (bump) is needed for a body to work')
   c_body.world = obm:get('bumpWorld')
   c_body.world:add(c_body,c_body.position.x,c_body.position.y,c_body.w,c_body.h)
 
-  function c_body:addVec(vector)
-    local actualX, actualY, cols, len = self.world:move(self,c_body.position.x+vector.x,c_body.position.y+vector.y,self.filter)
-    self.position.x = actualX
-    self.position.y = actualY
+  function c_body:moveCollide(vector)
+    --we try to move the body and return new coordinates
+    local actualX, actualY, cols, len = self.world:move(self,self.position.x+vector.x,self.position.y+vector.y,self.filter)
+    self.position = vec2(actualX,actualY)
+    return(vec2(actualX,actualY))
   end
 
   function c_body:tick(dt)
-    --TODO check here what collides with what
-
-    if self.owner.position then
-      self.owner.position.x = self.position.x
-      self.owner.position.y = self.position.y
-    end
   end
 
   function c_body:draw()
@@ -59,7 +54,7 @@ function C_body:new(owner,id,a)
 
   function c_body:filter(other)
     --interrogate colm to know the collision type
-    coltype = colm:getColType(self.family,other.family)
+    coltype = colm:getCollisionResponse(self.family,other.family)
     return coltype
   end
 
