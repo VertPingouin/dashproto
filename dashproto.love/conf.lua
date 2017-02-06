93c71b2eb27ad208839770c5ccbde1f30adf8b1f
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-02-06 08:53:19 +0100

    add native res and scaling factor

--- dashproto.love/conf.lua
+++ dashproto.love/conf.lua
@@ -4,7 +4,7 @@ function love.conf(t)
 	t.version = "0.10.0"         -- The LÖVE version this game was made for (string)
 	t.window.borderless = false
 	t.window.width = 1024
-	t.window.height = 768
+	t.window.height = 960
 
 	-- For Windows debugging
 	t.console = true
@@ -13,7 +13,14 @@ function love.conf(t)
 		boxes = true,
 		loglevel = 'DEBUG' --TODO make debug level work
 	}
+
+	resx = 256
+	resy = 240
+	multx = 1024 / 256
+	multy = 960 / 240
 	tilesize = 32
-	maxlayers = 10
+	maxlayer = 10
+	minlayer = 1
 	maxorders = 5
+	minorders = 1
 end
