8068dd5d1b42307fda360e7b2f0a162c324ca747
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-02-06 13:57:27 +0100

    use new c_body metamethods

--- dashproto.love/entities/camera.lua
+++ dashproto.love/entities/camera.lua
@@ -19,40 +19,37 @@ function Camera:new(parent,a)
   camera.target = a.target
   camera.boundaries = a.boundaries
   camera.position = vec2(0,0)
-  camera:add(c_body:new(camera,'viewport',{x=0,y=0,w=a.w,h=a.h,color=color:new(0,255,0,40),family='viewport',layer=minlayer}),'viewport')
+  camera:add(c_body:new(camera,'viewport',{x=0,y=0,w=a.w,h=a.h,color=color:new(0,0,255,0),family='viewport',layer=minlayer}),'viewport')
 
   function camera:setTarget(target)
   end
 
   function camera:oTick(dt)
     --center camera on object respecting the boundaries
-    --TODO camera follow might be done simpler
-    local targetcenter = self.target:getCenter()
-
-    local left = targetcenter.x - self.viewport.w / 2
-    if left > self.boundaries.position.x then
-      self.position.x = left
-      self.viewport.position.x = left
-    else
-      targetcenter.x = self.boundaries.position.x
-      self.viewport.position.x =  self.boundaries.position.x
+    self.viewport.center = self.target.center
+    if self.viewport.left < self.boundaries.left then
+      self.viewport.left = self.boundaries.left
     end
 
-    local top = targetcenter.y - self.viewport.h / 2
-    if top > self.boundaries.position.y then
-      self.position.y = top
-      self.viewport.position.y = top
-    else
-      targetcenter.y = self.boundaries.position.y
-      self.viewport.position.y =  self.boundaries.position.y
+    if self.viewport.right > self.boundaries.right then
+      self.viewport.right = self.boundaries.right
     end
+
+    if self.viewport.top < self.boundaries.top then
+      self.viewport.top = self.boundaries.top
+    end
+
+    if self.viewport.bottom > self.boundaries.bottom then
+      self.viewport.bottom = self.boundaries.bottom
+    end
+
+    self.position.x = self.viewport.left
+    self.position.y = self.viewport.top
   end
-
   function camera:oDraw()
     love.graphics.push()
-    love.graphics.translate(-self.viewport.position.x,-self.viewport.position.y)
+    love.graphics.translate(-self.position.x,-self.position.y)
   end
-
 
   return camera
 end
