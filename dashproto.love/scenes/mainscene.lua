56d5b9a89b2073603a4d1b8fdea86d6a250e6436
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-02-06 08:52:58 +0100

    add camera

--- dashproto.love/scenes/mainscene.lua
+++ dashproto.love/scenes/mainscene.lua
@@ -2,9 +2,9 @@ MainScene = {}
 
 function MainScene:new(parent)
   local mainScene = scene:new(parent,'mainScene')
-  mainScene.map = map:new('mainScene','mainSceneMap',{luamap=require('maps/test'),position=vec2(20,20)})
+  mainScene.map = map:new('mainScene','mainSceneMap',{luamap=require('maps/test'),position=vec2(0,0)})
   mainScene.player = player:new('mainScene',{position=mainScene.map:getSpawn('player')})
-
+  mainScene.camera = camera:new('mainScene',{h=240,w=320,target=mainScene.player.mainBody,boundaries=mainScene.map.boundaries})
   function mainScene:tick(dt)
   end
 
