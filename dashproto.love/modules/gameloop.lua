ec78e87622e0fb3c2c2a4017ab808cc3c36fb001
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-02-06 08:55:07 +0100

    added minorders

--- dashproto.love/modules/gameloop.lua
+++ dashproto.love/modules/gameloop.lua
@@ -5,7 +5,7 @@ end
 
 function GameLoop:tick(dt)
   --a function that will make every ticking entity tick
-  for i = 0,maxorders do
+  for i = minorders,maxorders do
     for j,ticking in ipairs(obm:getTicking(i)) do
       ticking:tick(dt)
     end
