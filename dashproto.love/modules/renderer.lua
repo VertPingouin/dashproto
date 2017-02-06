df599c5b6282808aa0ec4f812434becc8fadb130
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-02-06 08:55:20 +0100

    add minorders

--- dashproto.love/modules/renderer.lua
+++ dashproto.love/modules/renderer.lua
@@ -4,16 +4,26 @@ local remove = table.remove
 
 function Renderer:load()
   love.graphics.setDefaultFilter("nearest", "nearest")
+  Renderer.canvas = love.graphics.newCanvas(resx,resy)
 end
 
 
 function Renderer:draw()
   --a function that will draw visble layers by layers
-  for i = 0,maxlayers do
+  love.graphics.setCanvas(Renderer.canvas)
+  love.graphics.clear()
+  for i = minlayer,maxlayer do
     for j,visible in ipairs(obm:getVisible(i)) do
       visible:draw()
     end
+    if i == maxlayer-1 then --all layer but the last are scaled on canvas
+      love.graphics.setCanvas()
+      love.graphics.draw(Renderer.canvas,0,0,0,multx,multy)
+    elseif i == maxlayer-2 then --all layers but the two lasts are influenced by camera
+      love.graphics.pop()
+    end
   end
+
 end
 
 return Renderer
