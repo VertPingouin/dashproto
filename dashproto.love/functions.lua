5478b55571f331970bb308a52899cab38c25f3ea
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-02-06 08:53:38 +0100

    generic functions

---
+++ dashproto.love/functions.lua
@@ -0,0 +1,21 @@
+function tcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in pairs(orig) do
+            copy[orig_key] = orig_value
+        end
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
+function comparetables(t1, t2)
+  if #t1 ~= #t2 then return false end
+  for i=1,#t1 do
+    if t1[i] ~= t2[i] then return false end
+  end
+  return true
+end
