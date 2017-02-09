b05659c133dca62da7417143922452ffba1a6675
= <sylvain.boussekey@vertpingouin.fr>
2017-02-09 20:34:45 +0100

    added getQuad function

--- dashproto.love/modules/asm.lua
+++ dashproto.love/modules/asm.lua
@@ -8,17 +8,13 @@ function ASM:add(asset,id)
   self.assets[id] = asset
 end
 
-function ASM:getSprite(id,cell,celly)
+function ASM:getSprite(id,cell)
   assert(self.assets[id],'ASM::get::no sprite with id'..id)
   pic = self.assets[id].pic
 
-  if celly then
-    assert(self.assets[id].quads[celly][cell],'ASM::get::no quad with coordinates '..cell..'/'..celly)
-    quad = self.assets[id].quads[celly][cell]
-  else
-    assert(self.assets[id].quads[cell],'ASM::get::no quad with id '..id)
-    quad = self.assets[id].quads[cell]
-  end
+  assert(self.assets[id].quads[cell],'ASM::get::no quad with id '..cell)
+  quad = self.assets[id].quads[cell]
+
   return {pic=pic,quad=quad}
 end
 
@@ -26,6 +22,12 @@ function ASM:get(id)
   assert(self.assets[id],'ASM::get::no sprite with id'..id)
   return self.assets[id].pic
 end
+
+function ASM:getQuad(id,quad)
+  assert(self.assets[id],'ASM::get::no quad with id'..quad)
+  return self.assets[id][quad]
+end
+
 --get all quads at one time, useful for icons with three layers
 function ASM:quads(id)
   return self.assets[id].quads
