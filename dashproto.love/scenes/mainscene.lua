b089c763e8ea52558d355e49e64594d262007f24
= <sylvain.boussekey@vertpingouin.fr>
2017-02-09 20:35:12 +0100

    testing tilemap entity

--- dashproto.love/scenes/mainscene.lua
+++ dashproto.love/scenes/mainscene.lua
@@ -3,6 +3,12 @@ MainScene = {}
 function MainScene:new(parent)
   local mainScene = scene:new(parent,'mainScene')
   mainScene.map = map:new('mainScene','mainSceneMap',{luamap=require('maps/test'),position=vec2(0,0)})
+  mainScene.bg = tilemap:new('mainScene','bg',{luamap=require('maps/test'),asset='graveyard',position=vec2(0,0),layer=1})
+  mainScene.l1 = tilemap:new('mainScene','l1',{luamap=require('maps/test'),asset='graveyard',position=vec2(0,0),layer=2})
+  mainScene.l2 = tilemap:new('mainScene','l2',{luamap=require('maps/test'),asset='graveyard',position=vec2(0,0),layer=2})
+
+  mainScene.map2 = map:new('mainScene','mainSceneMap2',{luamap=require('maps/test'),position=vec2(32*16,0)})
+
   mainScene.player = player:new('mainScene',{position=mainScene.map:getSpawn('player')})
   mainScene.camera = camera:new('mainScene',{w=params.nativeresx,h=params.nativeresy,target=mainScene.player.mainBody,boundaries=mainScene.map.boundaries})
   function mainScene:tick(dt)
