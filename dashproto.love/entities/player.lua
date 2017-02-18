93f5ba4f70699454b2b7f5e995031034b9af884b
= <sylvain.boussekey@vertpingouin.fr>
2017-02-18 05:49:39 +0100

    testing

--- dashproto.love/entities/player.lua
+++ dashproto.love/entities/player.lua
@@ -18,17 +18,30 @@ function Player:new(parent,a)
   player.position = a.position
 
   player.movement = vec2(0,0)
+  player.hurtdir = vec2(1,1)
+  player.flicker = true
 
   --we get the joy1 (child of game) to be able to read the input
   player.joystick = obm:get('joy1')
 
   --a statemachine
   player:add(c_statemachine:new(player,'mainFSM'),'mainFSM')
-  player.mainFSM:addState('Idle',{enter='onEnterIdle'})
-  player.mainFSM:addState('Moving',{enter='onEnterMoving'})
+  player.mainFSM:addState('Idle',{enter='onEnterIdle',step='whileIdle'})
+  player.mainFSM:addState('Moving',{enter='onEnterMoving',step='whileMoving'})
+  player.mainFSM:addState('Hurt',{step='whileHurt',enter='onHurt'})
   player.mainFSM:addTransition('Idle','Moving')
   player.mainFSM:addTransition('Moving','Idle')
+  player.mainFSM:addTransition('Moving','Hurt')
+  player.mainFSM:addTransition('Idle','Hurt')
+  player.mainFSM:addTransition('Hurt','Idle',{preferred=true})
   player.mainFSM:setInitialState('Idle')
+
+
+  player:add(c_statemachine:new(player,'stateFSM'),'stateFSM')
+  player.stateFSM:addState('Vulnerable')
+  player.stateFSM:addState('Invulnerable')
+  player.stateFSM:addTransition('Vulnerable','Invulnerable',{preferred=true})
+  player.stateFSM:addTransition('Invulnerable','Vulnerable',{preferred=true,ttl=1})
 
   player:add(c_body:new(player,'mainBody',{
     x=player.position.x,
@@ -82,13 +95,30 @@ function Player:new(parent,a)
   --TODO make a statemachine for cooldown
   --TODO use step functions for statemachine
   function player:oTick(dt)
+  end
+
+  function player:whileIdle(dt)
+    if self.mainBody:collideFamily('ennemy') and self.stateFSM.currentState == 'Vulnerable' then
+      self.hurtdir = self.mainBody:collideFamily('ennemy')
+      self.mainFSM:transition('Hurt')
+     end
+
+    local left = self.joystick:get('left')
+    local right = self.joystick:get('right')
+    local up = self.joystick:get('up')
+    local down = self.joystick:get('down')
+
+    if left+right+up+down ~= 0 then self.mainFSM:transition('Moving') end
+
+  end
+
+  function player:whileMoving(dt)
     local left = self.joystick:get('left')
     local right = self.joystick:get('right')
     local up = self.joystick:get('up')
     local down = self.joystick:get('down')
 
     if left+right+up+down ~= 0 then
-      player.mainFSM:transition('Moving')
       self.movement.x = -left+right
       self.movement.y = -up+down
       if self.movement.x == 0 then
@@ -105,15 +135,31 @@ function Player:new(parent,a)
         end
       end
 
-      self:moveCollide(self.movement:normalizeInplace() * 75 * dt,self.mainBody)
+      self:moveCollide(self.movement:normalizeInplace() * 100 * dt,self.mainBody)
     else
       player.mainFSM:transition('Idle')
+    end
+    if self.mainBody:collideFamily('ennemy') and self.stateFSM.currentState == 'Vulnerable' then
+      self.hurtdir = self.mainBody:collideFamily('ennemy')
+      self.mainFSM:transition('Hurt')
+    end
+  end
+
+  function player:onHurt()
+    self.stateFSM:transition('Invulnerable')
+  end
+
+  function player:whileHurt(dt)
+    self:moveCollide(-self.hurtdir:normalizeInplace() * 150 * dt,self.mainBody)
+    if not self.mainBody:collideFamily('ennemy') then
+      self.mainFSM:transition('Idle')
     end
   end
 
   function player:moveCollide(vector,body)
     --moves the player according to a c_body collisions
     self.position = body:moveCollide(vector)
+    --TODO make all the body move with owner
   end
 
   function player:tpCollide(vector,body)
@@ -131,9 +177,16 @@ function Player:new(parent,a)
   end
 
   function player:oDraw()
+    if self.stateFSM.currentState == 'Invulnerable' then
+      self.flicker=not self.flicker
+      self.mainSprite.visible = self.flicker
+    else
+      self.mainSprite.visible = true
+    end
   end
 
   player.mainFSM:initialize('Idle')
+  player.stateFSM:initialize('Vulnerable')
   return player
 end
 
