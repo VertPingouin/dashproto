06270ddc9fc250a0f1a56862380d458b8f6d8413
= <sylvain.boussekey@vertpingouin.fr>
2017-02-21 17:08:38 +0100

    added whip sprite

--- dashproto.love/ressources.lua
+++ dashproto.love/ressources.lua
@@ -1,3 +1,4 @@
 asm:add(asset:newSpritesheet(require('assets/spritesheets/player')),'player')
 asm:add(asset:newSpritesheet(require('assets/spritesheets/skeleton')),'skeleton')
 asm:add(asset:newSpritesheet(require('assets/spritesheets/graveyard')),'graveyard')
+asm:add(asset:newSpritesheet(require('assets/spritesheets/whip')),'whip')
