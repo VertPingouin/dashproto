c2817ede08b80102fa39c3ae6d97c922a23b2245
= <sylvain.boussekey@vertpingouin.fr>
2017-02-24 05:58:59 +0100

    remove todo

--- dashproto.love/datamodel/color.lua
+++ dashproto.love/datamodel/color.lua
@@ -4,7 +4,6 @@ function Color:new(r,g,b,a)
   assert(r and g and b and a,'Color::new::rgba values are needed (yes, all four !)')
   local color = {r=r,g=g,b=b,a=a}
 
-  --TODO see if love.graphics.setcolor has its place here
   return color
 end
 
