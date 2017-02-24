fdc0499962c4f8c4eee30ba5bdd0c04d0f3df0a5
= <sylvain.boussekey@vertpingouin.fr>
2017-02-24 05:56:35 +0100

    changing skeleton speed and behavior

--- dashproto.love/entities/skeleton.lua
+++ dashproto.love/entities/skeleton.lua
@@ -20,16 +20,21 @@ function Skeleton:new(parent,a)
   skeleton.position = a.position
   skeleton.speed = 20
   skeleton.movement = vec2(1,1)
+  skeleton.hp = 3
 
   --a statemachine
   skeleton:add(c_statemachine:new(skeleton,'behavior'),'behavior')
   skeleton.behavior:addState('Agressive',{enter='onEnterAgressive',step='whileAgressive'})
   skeleton.behavior:addState('Wandering',{enter='onEnterWandering',step='whileWandering'})
   skeleton.behavior:addState('Retreating',{enter='onEnterRetreating',step='whileRetreating'})
+  skeleton.behavior:addState('Dying',{enter='onEnterDying'})
+  skeleton.behavior:addState('Dead',{enter='onEnterDead'})
   skeleton.behavior:addTransition('Agressive','Wandering')
   skeleton.behavior:addTransition('Wandering','Agressive')
   skeleton.behavior:addTransition('Agressive','Retreating')
-  skeleton.behavior:addTransition('Retreating','Wandering',{preferred=true,ttl=.8})
+  skeleton.behavior:addTransition('Retreating','Wandering',{preferred=true,ttl=.2})
+  skeleton.behavior:addTransition('Retreating','Dying')
+  skeleton.behavior:addTransition('Dying','Dead',{preferred=true,ttl=.9})
   skeleton.behavior:setInitialState('Wandering')
 
   skeleton:add(c_body:new(skeleton,'mainBody',{
@@ -68,8 +73,8 @@ function Skeleton:new(parent,a)
   vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
     vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
     pixel.r = pixel.r + amount;
-    pixel.g = pixel.g+ amount;
-    pixel.b = pixel.b+ amount;
+    pixel.g = pixel.g + amount;
+    pixel.b = pixel.b + amount;
     return pixel;
   }
   ]]}))
@@ -107,7 +112,14 @@ function Skeleton:new(parent,a)
     frames = {'11-12',1,'11-10',1},
     durations = .1
   })
-
+  skeleton.mainSprite:add({
+    name = 'skeleton_die',
+    pic = asm:get('skeleton'),
+    cellsizex = 16,
+    cellsizey = 24,
+    frames = {3,1,'13-14',1},
+    durations = .3
+  })
   skeleton.mainSprite:setAnimation('skeleton_walk_down')
 
   function skeleton:onEnterWandering()
@@ -115,25 +127,33 @@ function Skeleton:new(parent,a)
   end
 
   function skeleton:onEnterAgressive()
-    self.speed = 30
+    self.speed = 70
   end
 
-  function skeleton:whileWandering()
+  function skeleton:whileWandering(dt)
+    local vec,col = self:moveCollide(self.movement:normalizeInplace() * self.speed * dt,self.mainBody)
+    if col>0 then self.movement = self.movement:perpendicular() end
+    self.mainSprite:setAnimation('skeleton_walk_'..cardinalDir(self.movement:normalizeInplace()))
     local dice = math.random(0, 50)
     if dice == 1 then self:rerollDirection() end
     if self.evilLook:see() then self.behavior:transition('Agressive') end
   end
 
-  function skeleton:whileAgressive()
+  function skeleton:whileAgressive(dt)
+    local vec,col = self:moveCollide(self.movement:normalizeInplace() * self.speed * dt,self.mainBody)
+    if col>0 then self.movement = self.movement:perpendicular() end
+    self.mainSprite:setAnimation('skeleton_walk_'..cardinalDir(self.movement:normalizeInplace()))
     local look = self.evilLook:see()
     if look then
       self.movement = look:normalizeInplace()
       if self.mainBody:collideName('player.mainBody') then
-        self.speed = 20
+        self.speed = 100
         self.behavior:transition('Retreating')
       elseif self.mainBody:collideName('player.hitBox') then
         self.whiteflash:play()
-        self.speed = 40
+        self.hp = self.hp-1
+        obm:get('camera'):shake(.1,1)
+        self.speed = 100
         self.behavior:transition('Retreating')
       end
     else
@@ -141,10 +161,28 @@ function Skeleton:new(parent,a)
     end
   end
 
-  function skeleton:onEnterRetreating()
+  function skeleton:onEnterDying()
   end
 
-  function skeleton:whileRetreating()
+  function skeleton:onEnterDead()
+    self:destroy()
+  end
+
+  function skeleton:onEnterRetreating()
+    if self.hp == 0 then
+      self.movement = vec2(0,0)
+      self.hurtBody:setActive(false)
+      self.mainBody:setActive(false)
+      self.mainSprite:setAnimation('skeleton_die')
+      self.behavior:transition('Dying')
+    end
+  end
+
+  function skeleton:whileRetreating(dt)
+    local vec,col = self:moveCollide(self.movement:normalizeInplace() * self.speed * dt,self.mainBody)
+    if col>0 then self.movement = self.movement:perpendicular() end
+    self.mainSprite:setAnimation('skeleton_walk_'..cardinalDir(-self.movement:normalizeInplace()))
+
     local look = self.evilLook:see()
     if look then
       self.movement = -look:normalizeInplace()
@@ -154,28 +192,6 @@ function Skeleton:new(parent,a)
   end
 
   function skeleton:oTick(dt)
-    local angle = 0
-    if self.behavior.currentState == 'Retreating' then
-      angle = self.movement:normalizeInplace():angleTo(vec2(0,1))
-    else
-      angle = self.movement:normalizeInplace():angleTo(vec2(0,-1))
-    end
-
-    if (angle < 0) then angle = angle + 2 * pi end
-
-    if angle > 7*pi/4 or angle <= pi/4 then
-      self.mainSprite:setAnimation('skeleton_walk_up')
-    elseif pi/4 < angle and angle <= 3*pi/4 then
-      self.mainSprite:setAnimation('skeleton_walk_right')
-    elseif 3*pi/4 < angle and angle <= 5*pi/4 then
-      self.mainSprite:setAnimation('skeleton_walk_down')
-    elseif 5*pi/4 < angle and angle <= 7*pi/4 then
-      self.mainSprite:setAnimation('skeleton_walk_left')
-    end
-
-    local vec,col = self:moveCollide(self.movement:normalizeInplace() * self.speed * dt,
-      self.mainBody)
-    if col>0 then self.movement = self.movement:perpendicular() end
   end
 
   function skeleton:rerollDirection()
