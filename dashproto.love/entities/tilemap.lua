cca0f23398208a138ecd6ebb0389d4afaa4c8e52
= <sylvain.boussekey@vertpingouin.fr>
2017-02-25 21:57:29 +0100

    add layername parameter

--- dashproto.love/entities/tilemap.lua
+++ dashproto.love/entities/tilemap.lua
@@ -1,19 +1,21 @@
+--reads tlm files and create large sprites based on tiles to be displayed ingame
 local insert = table.insert
 
 TileMap = {}
 
-function TileMap:new(parent,name,a)
+function TileMap:new(parent,a)
   local check = acheck:new()
   check:add({
     {'luamap','mandatory','table'},
     {'position','defaultValue','table',vec2(0,0)},
+    {'layername','mandatory','string'},
     {'layer','mandatory','number'},
     {'asset','mandatory','string'}
   })
   a=check:check(a)
 
   local tilemap = entity:new({
-    name=name,
+    name=parent..'.'..a.layername,
     parent=parent,
     tags={'ticking','visible'},
     layer = a.layer
@@ -29,7 +31,7 @@ function TileMap:new(parent,name,a)
   tilemap.pic = love.graphics.newCanvas(tilemap.width*tilemap.tilewidth, tilemap.height*tilemap.tileheight)
 
   for i,layer in ipairs(a.luamap.layers) do
-    if layer.type == 'tilelayer' and layer.name == name then
+    if layer.type == 'tilelayer' and layer.name == a.layername then
       love.graphics.setCanvas(tilemap.pic)
       for j,cell in ipairs(layer.data) do
         if cell ~= 0 then
