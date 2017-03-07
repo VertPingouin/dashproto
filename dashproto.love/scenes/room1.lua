a498b25bed8aeba64292f37095a1e345ceb077dd
= <sylvain.boussekey@vertpingouin.fr>
2017-03-07 05:55:43 +0100

    use new tlm entity

--- dashproto.love/scenes/room1.lua
+++ dashproto.love/scenes/room1.lua
@@ -1,19 +1,22 @@
 Room1 = {}
-function Room1:load(parent,playerSpawn)
+function Room1:new(parent,playerSpawn)
 
   local room1 = scene:new(parent,'room1')
-  room1.room = tlm:new('room1',{
-    luamap='graveyard',
-    asset='graveyard',
-    playerSpawn=playerSpawn
-  })
 
-  room1.camera = camera:new('room1',{
-    w=params.nativeresx,
-    h=params.nativeresy,
-    target=obm:get('player').mainBody,
-    boundaries=obm:get('graveyard.map').boundaries
-  })
+  function room1:oLoad()
+    room1.room = tlm:new('room1',{
+      luamap='graveyard',
+      asset='graveyard',
+      playerSpawn=playerSpawn
+    })
+
+    room1.camera = camera:new('room1',{
+      w=params.nativeresx,
+      h=params.nativeresy,
+      target=obm:get('player').mainBody,
+      boundaries=obm:get('graveyard.map').boundaries
+    })
+  end
 
   function room1:tick(dt)
   end
