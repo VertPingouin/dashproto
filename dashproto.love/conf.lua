961d458ccc31ba9dc623a5cd395b2bf7994656b5
= <sylvain.boussekey@vertpingouin.fr>
2017-03-07 05:56:11 +0100

    testing

--- dashproto.love/conf.lua
+++ dashproto.love/conf.lua
@@ -1,32 +1,31 @@
 -- Configuration
 function love.conf(t)
+
 	params = {
-	maxfps = 50,
+	maxfps = 30,
 	nativeresx = 256,
 	nativeresy = 240,
-	multx = 3,
-	multy = 3,
-	resx = 768,
-	resy = 720,
 	tilesize = 16,
 	maxlayer = 10,
 	minlayer = 1,
 	maxorder = 5,
 	minorder = 1,
+	limitfps = true,
 	debug = {
-		boxes = false,
+		boxes = true,
 		fps = true,
-		console = false,
+		console = true,
 		look = false,
-		log = true
+		log = true,
 		}
 	}
+
 
 	t.title = "Dash proto" -- The title of the window the game is in (string)
 	t.version = "0.10.0"         -- The LoVE version this game was made for (string)
 	t.window.borderless = true
-	t.window.width = 768
-	t.window.height = 720
+	--t.window.width = 768
+	--t.window.height = 720
 
 	-- For Windows debugging
 	t.console = true
