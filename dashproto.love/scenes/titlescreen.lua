84566123f8e3b25525706d335c6cff47948644d5
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-03-07 16:07:17 +0100

    correct bug in titlescreen

--- dashproto.love/scenes/titlescreen.lua
+++ dashproto.love/scenes/titlescreen.lua
@@ -2,9 +2,18 @@ TitleScreen = {}
 function TitleScreen:new(parent)
 
   local titlescreen = scene:new(parent,'titlescreen')
-  titlescreen.sprite = sprite:new('titlescreen',{assetname='title'})
 
-  titlescreen.camera = nullcam:new('titlescreen')
+  titlescreen.camera=nullcam:new('titlescreen')
+  titlescreen:add(c_sprite:new('title','blink'))
+  titlescreen.blink:add({
+    name = 'blink',
+    pic = asm:get('title'),
+    cellsizex = 256,
+    cellsizey = 240,
+    frames = {'1-2',1},
+    durations = .3
+  })
+  titlescreen.blink:setAnimation('blink')
 
   return titlescreen
 end
