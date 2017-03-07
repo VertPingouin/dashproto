f6efd1cb8d08ff256d40585652b03825e44dff17
= <sylvain.boussekey@vertpingouin.fr>
2017-03-07 05:59:42 +0100

    a colored rectangle

---
+++ dashproto.love/components/c_rectangle.lua
@@ -0,0 +1,36 @@
+C_rectangle = {}
+function C_rectangle:new(owner,id,a)
+  local c_rectangle = component:new(owner,id,a)
+
+  local check = acheck:new()
+  check:add({
+    {'position','mandatory','table'},
+    {'size','mandatory','table'},
+    {'color','defaultValue','table',color:new(255,255,255,255)}
+  })
+  a = check:check(a)
+
+  c_rectangle.position = a.position
+  c_rectangle.size = a.size
+  c_rectangle.color = a.color
+
+  function c_rectangle:draw()
+    love.graphics.setColor(
+      self.color.r,
+      self.color.g,
+      self.color.b,
+      self.color.a
+    )
+    love.graphics.rectangle( 'fill',
+      self.position.x,
+      self.position.y,
+      self.size.x,
+      self.size.y
+    )
+    love.graphics.setColor(255,255,255,255)
+  end
+
+  return c_rectangle
+end
+
+return C_rectangle
