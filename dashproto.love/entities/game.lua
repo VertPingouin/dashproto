2d8a728e39667854e62c914fb7e41fac972cc37e
= <sylvain.boussekey@vertpingouin.fr>
2017-03-07 05:57:29 +0100

    implemented a game state

--- dashproto.love/entities/game.lua
+++ dashproto.love/entities/game.lua
@@ -14,42 +14,78 @@ function Game:new()
     right = {'axis:leftx+', 'button:dpright','key:right'},
     up = {'axis:lefty-', 'button:dpup','key:up'},
     down = {'axis:lefty+', 'button:dpdown','key:down'},
-    hit = {'button:5','key:space'}
+    hit = {'button:5','key:space'},
+    pause = {'button:start','key:p'},
+    f1 = {'key:f1'},
+    f2 = {'key:f2'},
+    f3 = {'key:f3'},
+    escape = {'key:escape'}
   }
 
   game.joy1 = baton.new('joy1','game',controls,love.joystick.getJoysticks()[1])
   game.playerHp = 10
   game.currentScene = nil
+  game.paused = false
+  game.black = black:new('game',{name='black'})
 
-  --unload current scene and load a new, optionally spawn the player on a certain spawnpoint
-  function game:setScene(scene, playerSpawn)
-    if self.currentScene then
-      self.currentScene:unload()
-      self.currentScene = nil
-    end
-    self.currentScene = require('scenes/'..scene):load('game',playerSpawn)
+  game:add(c_statemachine:new(game,'state'),'state')
+
+  game.state:addState('Title',{step='whileTitle',enter='onEnterTitle'})
+  game.state:addState('Game',{step='whileGame',enter='onEnterGame'})
+  game.state:addTransition('Title','Game')
+  game.state:addTransition('Game','Title')
+
+  game.state:setInitialState('Title')
+
+  function game:onEnterTitle()
+    self:setScene(require('scenes/titlescreen'))
   end
 
-  function game:oTick(dt)
+  function game:whileTitle(dt)
+    if self.joy1:pressed('pause') then
+      self:setScene('room1','player')
+      self.state:transition('Game')
+    end
+  end
+
+  function game:onEnterGame()
+  end
+
+  function game:whileGame(dt)
     --if player collides a passage, goto the proper scene with proper position
     local passage = obm:get('player').mainBody:getCollideFamily('passage')
     if passage then
       self:setScene(passage.destination,passage.spawn)
     end
 
-    --display debug infos
-    function love.keypressed(key,unicode)
-      if key == 'f1' then
-        obm:printChildren('root')
-      elseif key == 'f2' then
-        obm:printVisible()
-      elseif key == 'f3' then
-        obm:printTicking()
-      elseif key == 'f4' then
-        evm:post('test')
-      elseif key == 'escape' then
-        love.event.quit()
-      end
+    if self.joy1:pressed('pause') then
+      self.paused = not self.paused
+      obm:callByTag('pauseable','setPause',{self.paused})
+    end
+  end
+
+  --unload current scene and load a new, optionally spawn the player on a certain spawnpoint
+  --TODO intermediate scene to help load unload on the pi
+  function game:setScene(scene, playerSpawn)
+    self.black:on(.5)
+    if self.currentScene then
+      self.currentScene:unload()
+      self.currentScene = nil
+    end
+    self.currentScene = require('scenes/'..scene):new('game',playerSpawn)
+    self.currentScene:load()
+    obm:callByTag('pauseable','freeze',{.5})
+  end
+
+  function game:oTick(dt)
+    if self.joy1:pressed('f1') then
+      obm:printChildren('root')
+    elseif self.joy1:pressed('f2') then
+      obm:printVisible()
+    elseif self.joy1:pressed('f3') then
+      obm:printTicking()
+    elseif self.joy1:pressed('escape') then
+      love.event.quit()
     end
   end
 
