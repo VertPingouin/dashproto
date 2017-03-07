e4cfaccefbc75d096fb361798b9116da6c44e63a
BOUSSEKEY Sylvain <sylvain.boussekey@fr.adecco.net>
2017-03-07 16:07:55 +0100

    new ressources

--- dashproto.love/ressources.lua
+++ dashproto.love/ressources.lua
@@ -3,3 +3,4 @@ asm:add(asset:newSpritesheet(require('assets/spritesheets/skeleton')),'skeleton'
 asm:add(asset:newSpritesheet(require('assets/spritesheets/graveyard')),'graveyard')
 asm:add(asset:newSpritesheet(require('assets/spritesheets/whip')),'whip')
 asm:add(asset:newSpritesheet(require('assets/spritesheets/title')),'title')
+asm:add(asset:newSpritesheet(require('assets/spritesheets/gameover')),'gameover')
