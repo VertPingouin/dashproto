0c90a107ca579f030e1f773ed7bbfd6e1fedac80
= <sylvain.boussekey@vertpingouin.fr>
2017-03-22 21:25:03 +0100

    new assets

---
+++ dashproto.love/assets/spritesheets/s_redbone.lua
@@ -0,0 +1,7 @@
+return {
+  pic =love.graphics.newImage('assets/pics/redbone.png'),
+  cellsizex = 16,
+  cellsizey = 16,
+  nbcellx = 2,
+  nbcelly = 1
+}
---
+++ dashproto.love/assets/spritesheets/s_redskeleton.lua
@@ -0,0 +1,27 @@
+return {
+  pic =love.graphics.newImage('assets/pics/redskeleton.png'),
+  cellsizex = 16,
+  cellsizey = 24,
+  nbcellx = 18,
+  nbcelly = 1,
+  cellnames = {
+    'down1',
+    'down2',
+    'down3',
+    'up1',
+    'up2',
+    'up3',
+    'right1',
+    'right2',
+    'right3',
+    'left1',
+    'left2',
+    'left3',
+    'collapse1',
+    'collapse2',
+    'throwdown',
+    'throwup',
+    'throwright',
+    'throwleft'
+  }
+}
