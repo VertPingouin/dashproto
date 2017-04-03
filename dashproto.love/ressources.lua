1301423a5436a4c23ce894c9dfd73be677d74fe5
= <sylvain.boussekey@vertpingouin.fr>
2017-04-03 19:55:54 +0200

    add looped sounds

--- dashproto.love/ressources.lua
+++ dashproto.love/ressources.lua
@@ -9,10 +9,19 @@ asm:add(asset:newSpritesheet(require('assets/spritesheets/s_lifebar')),'lifebar'
 asm:add(asset:newSpritesheet(require('assets/spritesheets/s_redbone')),'redbone')
 
 --sounds
-local sounds = love.filesystem.getDirectoryItems('sounds')
+local sounds = love.filesystem.getDirectoryItems('sounds/effects')
 for i,sound in ipairs(sounds) do
   sound = split(sound,'.')
   if sound[2] and sound[2] == 'wav' then
-    soundm:add('sounds/'..sound[1]..'.'..sound[2],sound[1])
+    soundm:add('sounds/effects/'..sound[1]..'.'..sound[2],sound[1])
+  end
+end
+
+--musics
+local sounds = love.filesystem.getDirectoryItems('sounds/musics')
+for i,sound in ipairs(sounds) do
+  sound = split(sound,'.')
+  if sound[2] and sound[2] == 'wav' then
+    soundm:addLoop('sounds/musics/'..sound[1]..'.'..sound[2],sound[1])
   end
 end
